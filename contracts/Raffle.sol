e848d6e26e86e33774663be2c97827556dfb6c9a
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-09-21 16:27:13 +0100

    Implementing Chainlink VRF - The Request

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,20 +7,38 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
 
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle is VRFConsumerBaseV2{
+contract Raffle is VRFConsumerBaseV2 {
     // State Variable
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     // Events
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -28,24 +46,30 @@ contract Raffle is VRFConsumerBaseV2{
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
-	s_players.push(payable(msg.sender));
-	// Emit an event when we update a dynamic array or mapping
-	// Named events with the named events reversed
-	emit RaffleEnter(msg.sender);
-
+        s_players.push(payable(msg.sender));
+        // Emit an event when we update a dynamic array or mapping
+        // Named events with the named events reversed
+        emit RaffleEnter(msg.sender);
     }
 
-    function requestRandomWinner()external{
-	// Request the random number
-	// Once we get it, do something with it
-	// 2 transaction process 
+    function requestRandomWinner() external {
+        // Request the random number
+        // Once we get it, do something with it
+        // 2 transaction process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, //gasLane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
-
-    }
-
-
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     // View / Pure Functions
     function getEntraceFee() public view returns (uint256) {
