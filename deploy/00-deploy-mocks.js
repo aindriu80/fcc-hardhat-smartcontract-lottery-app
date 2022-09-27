e509401c8d0d591eb426da2e26213be186b67cdf
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-09-27 14:49:17 +0100

    Deploying Raffle.sol Mock Chainlink VRF Coordinator

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,12 +1,27 @@
 const { developmentChains } = require('../helper-hardhat-config')
 
+const BASE_FEE = ethers.utils.parseEther('0.25') //0.25 is the premium. It costs 0.25 LINK per request
+const GAS_PRICE_LINK = 1e9 // 100000000 // link per gas. Calcuated value based on the gas price of the chain.
+
+// Eth price $1,000,000.00
+// Chainlink Nodes pay the gas fees to give us randomness to give us randomness & do external exeutino
+// So the price of requsts chainge based on the price of gas
+
 module.exports = async function ({ getNamedAccount, deployments }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
-  const chainId = network.config.chainId
+  const args = [BASE_FEE, GAS_PRICE_LINK]
 
   if (developmentChains.includes(network.name)) {
     log('local network detected! Deploying mocks...')
     //  deploy a mock vrfcoordinator...
+    await deploy('VRFCoordinatorV2Mock', {
+      from: deployer,
+      log: true,
+      args: args,
+    })
+    log('Mocks deployed')
+    log('------------------------------------------------------------------')
   }
 }
+module.exports.tags = ['all', 'mocks']
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,8 +1,17 @@
 const { network } = require('hardhat')
+const { developmentChains } = require('../helper-hardhat-config')
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
+  let vrfCoordinatorV2Address
 
+  if (deploymentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract('VRFCoodinatorV2Mock')
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+  }
+
+  const args = []
   const raffle = await deploy('Raffle', {
     from: deployer,
     args: [],
