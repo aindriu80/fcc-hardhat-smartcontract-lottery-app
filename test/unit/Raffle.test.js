db0398959c6a056eab50e9d4eba0e1a84b9942b8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-10-03 16:22:35 +0100

    Adding the 3rd test

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -5,13 +5,14 @@ const { developmentChains, networkConfig } = require('../../helper-hardhat-confi
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe('Raffle Unit Tests', async function () {
-      let raffle, vrfCoordinatorV2Mock
+      let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer
       const chainId = network.config.chainId
 
       beforeEach(async function () {
-        const { deployer } = await getNamedAccounts()
+        deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(['all'])
         raffle = await ethers.getContract('Raffle', deployer)
+        raffleEntranceFee = await raffle.getEntraceFee()
         vrfCoordinatorV2Mock = await ethers.getContract('VRFCoordinatorV2Mock', deployer)
       })
       describe('constructor', async function () {
@@ -24,9 +25,15 @@ const { developmentChains, networkConfig } = require('../../helper-hardhat-confi
         })
       })
 
-      describe('enterRaffle', async function () {
+      describe('enterRaffle', function () {
         it("Reverts when you don't pay enough", async function () {
           await expect(raffle.enterRaffle()).to.be.revertedWith('Raffle__NotEnoughETHEntered')
+        })
+
+        it('records players when they enter', async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee })
+          const playerFromContract = await raffle.getPlayer(0)
+          assert.equal(playerFromContract, deployer)
         })
       })
     })
