5ce07ee0424957be5a6de51210f2c10ea29dcb28
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-10-25 20:42:53 +0100

    A code fix

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,9 +7,9 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
-import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
-import '@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol';
 
 error Raffle__NotEnoughETHEntered();
 error Raffle_TransferredFailed();
--- scripts/mockOffChain.js
+++ scripts/mockOffChain.js
@@ -15,6 +15,14 @@ async function mockKeepers() {
       console.log('No upkeep needed!')
     }
   }
+  async function mockVrf(requestId, raffle) {
+    console.log("We on a local network? Ok let's pretend...")
+    const vrfCoordinatorV2Mock = await ethers.getContract('VRFCoordinatorV2Mock')
+    await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, raffle.address)
+    console.log('Responded!')
+    const recentWinner = await raffle.getRecentWinner()
+    console.log(`The winner is: ${recentWinner}`)
+  }
 }
 mockKeepers()
   .then(() => process.exit(0))
